ee6f5864edee7489bdb3acd9e5ad10e1edfb59fa
kaddy120 <kaddy120@gmail.com>
2023-09-08 16:39:49 +0200

    add ship to gameboard

---
+++ gameboard.js
@@ -0,0 +1,67 @@
+class Gameboard {
+  constructor() {
+    /* this._gameboard  */
+    this._createdboard();
+    this._ships = [];
+  }
+
+  addShip(ship, startPosition, direction) {
+    if (!this._isPositionAvailable(startPosition, direction, ship.length())) {
+      return false;
+    }
+
+    const X = startPosition.x;
+    const Y = startPosition.y;
+    this._ships.push(ship);
+
+    if (direction === 'horizontal') {
+      for (let x = X; x < X + ship.length(); x++) {
+        this._board[x][Y] = { shoot: null, ship };
+      }
+    } else if ((direction = 'vertical')) {
+      for (let y = Y; y < Y + ship.length(); y++) {
+        this._board[X][y] = { shoot: null, ship };
+      }
+    }
+    return true;
+  }
+
+  _isPositionAvailable(startPosition, direction, length) {
+    const X = startPosition.x;
+    const Y = startPosition.y;
+
+    if (direction === 'horizontal') {
+      if (X + length >= 10) return false;
+
+      for (let x = X; x < X + length; x++) {
+        if (this._board[x][Y].ship) return false;
+      }
+    } else if ((direction = 'vertical')) {
+      if (Y + length >= 10) return false;
+
+      for (let y = Y; y < Y + length; y++) {
+        if (this._board[X][y].ship) return false;
+      }
+    }
+    return true;
+  }
+
+  getBoard = () => this._board;
+
+  receiveAttack(x, y) {}
+
+  _createdboard() {
+    const rows = 10;
+    const columns = 10;
+    this._board = [];
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+      for (let j = 0; j < columns; j++) {
+        row.push({ shoot: null, ship: null });
+      }
+      this._board.push(row);
+    }
+  }
+}
+
+module.exports = Gameboard;
--- index.test.js
+++ index.test.js
@@ -28,3 +28,76 @@ test('ship sink if hit as much as ship length', () => {
   expect(ship1.isSunk()).toBeTruthy();
 });
 
+test('board is 10x10', () => {
+  const gameboard = new Gameboard();
+  const board = gameboard.getBoard();
+  expect(board.length).toBe(10);
+  expect(board[0].length).toBe(10);
+});
+
+test('return true when ship is added', () => {
+  const gameboard = new Gameboard();
+
+  let startPosition = {
+    x: 0,
+    y: 0,
+  };
+
+  let direction = 'horizontal';
+  const length = ShipLength.Cruiser;
+  const ship = new Ship(length);
+
+  /* add a ship horizontally*/
+  expect(gameboard.addShip(ship, startPosition, direction)).toBeTruthy()
+
+  /* add a ship vertically*/
+  startPosition.y = 1
+  direction = 'vertical'
+  expect(gameboard.addShip(ship, startPosition, direction)).toBeTruthy()
+});
+
+test('return false when adding a ship that is going out of the board', () => {
+  const gameboard = new Gameboard();
+
+  let startPosition = {
+    x: 5,
+    y: 5,
+  };
+
+  let direction = 'horizontal';
+  const length = ShipLength.Carrier; // length is 5
+  const ship = new Ship(length);
+
+  expect(gameboard.addShip(ship, startPosition, 'horizontal')).toBeFalsy()
+  startPosition.x = 4;
+  expect(gameboard.addShip(ship, startPosition, 'vertical')).toBeFalsy()
+});
+
+test('return false when ships are overlaping', () => {
+  const gameboard = new Gameboard();
+
+  let startPosition = {
+    x: 0,
+    y: 0,
+  };
+
+  let direction = 'horizontal';
+  const length = ShipLength.Cruiser;
+  const ship = new Ship(length);
+
+  gameboard.addShip(ship, startPosition, direction)
+  expect(gameboard.addShip(ship, startPosition, 'vertical')).toBeFalsy()
+
+  startPosition.x += length - 1;
+  expect(gameboard.addShip(ship, startPosition,direction)).toBeFalsy()
+});
+
+test('return true if received attack hit a Ship', () => {});
+
+test('return false if received attack hit a Ship', () => {});
+
+test('return -1 if attacing a position already attacked', () => {});
+
+test('get a number of opperating ships', () => {});
+
+test('get a number of opperating ships after sunking one', () => {});
