b6a7ed8738fbc39403adf7c1a3c51075ae7dcfc0
kaddy120 <kaddy120@gmail.com>
2023-09-20 00:59:01 +0200

    fix unhilighting bug when you add ship where some squares would remain highlighted

--- src/splashScreen.js
+++ src/splashScreen.js
@@ -7,13 +7,15 @@ class SplashScreen {
 
   shipNames = Object.keys(ShipLength);
 
-  addingShip = 0;
+  shipNumber = 0;
 
   rotateAxis = document.querySelector('#rotate-axis');
 
   player1Squares = document.querySelectorAll('.player-1 .square');
 
   player2Board = document.querySelector('.player-2');
+
+  shipName = document.querySelector('.ship-name');
 
   constructor(game) {
     this.game = game;
@@ -28,7 +30,7 @@ class SplashScreen {
     this.player1Squares.forEach((square) => {
       square.addEventListener('mouseover', () => {
         const position = Square.parse(square);
-        const name = this.shipNames[this.addingShip];
+        const name = this.shipNames[this.shipNumber];
         Square.highLightSquares(
           position,
           this.direction,
@@ -43,7 +45,7 @@ class SplashScreen {
 
       square.addEventListener('mouseleave', () => {
         const position = Square.parse(square);
-        const name = this.shipNames[this.addingShip];
+        const name = this.shipNames[this.shipNumber];
         Square.highLightSquares(
           position,
           this.direction,
@@ -62,7 +64,8 @@ class SplashScreen {
 
   handleAddShip(event) {
     const position = Square.parse(event.target);
-    const name = this.shipNames[this.addingShip];
+    const name = this.shipNames[this.shipNumber];
+    this.shipName.innerHTML = name;
 
     const ship = new Ship(ShipLength[name], name);
     if (this.game.addShip(ship, position, this.direction)) {
@@ -74,12 +77,31 @@ class SplashScreen {
       };
       const player1Water = document.querySelector('.player-1');
       player1Water.appendChild(shipIcon(imgMetadata));
-      this.addingShip += 1;
 
-      // remove event listener after adding all ships
-      if (this.addingShip === 5) {
+      // redudently unhilight the squares (of ship length) before shipNumber is incremented
+      // below. Because once shipNumber changes, the ship length we can access with this
+      // value will also change, resulting in some squares not getting unhilighted in
+      // mouseleave eventhandler.
+      Square.highLightSquares(
+        position,
+        this.direction,
+        ShipLength[name],
+        (x, y, className = 'highlight') => {
+          document
+            .querySelector(`.player-1 [data-index="${y * 10 + x}"]`)
+            .classList.remove(className);
+        },
+      );
+
+      this.shipNumber += 1;
+
+      // after adding all 5 ships, do:
+      if (this.shipNumber === 5) {
+        // 1. hide and show ui for adding ship and enemy board, respectively.
         document.querySelector('.placing-ship').classList.add('hide');
         this.player2Board.classList.remove('hide');
+
+        // 2. remove click event listner for adding ships in every square.
         this.player1Squares.forEach((square) => {
           square.removeEventListener('click', this.handleAddShip);
         });
