3fbf826600c4a670a90f9360ab56b06ddd56295c
kaddy120 <kaddy120@gmail.com>
2023-09-23 13:38:01 +0200

    ensure that player one can't play when a bot is thinking

--- src/screenController.js
+++ src/screenController.js
@@ -9,6 +9,8 @@ class ScreenController {
 
   waterContainer = document.createElement('div');
 
+  #botThinking = false;
+
   constructor() {
     this.waterContainer.classList.add('water-container');
     this.waterContainer.appendChild(Square.createBoard('player-1'));
@@ -22,7 +24,10 @@ class ScreenController {
 
   play() {
     this.player2Squares.forEach((square) => {
-      square.addEventListener('click', () => {
+      square.addEventListener('click', async () => {
+        // ensures that player1 cannot play twice when the bot is thinking
+        if (this.#botThinking) return;
+
         const position = Square.parse(square);
         const shoot = this.game.play(position);
         if (shoot.human.status === 'x') {
@@ -32,7 +37,7 @@ class ScreenController {
           // eslint-disable-next-line no-param-reassign
           square.innerHTML = 'o';
         }
-
+        await this.thinking(1000);
         const botShoot = shoot.bot;
         const postion = { x: botShoot.x, y: botShoot.y };
         const botSquareShoot = Square.findSquare('player-1', postion);
@@ -40,10 +45,16 @@ class ScreenController {
       });
     });
   }
+
+  async thinking(ms) {
+    this.#botThinking = true;
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        this.#botThinking = false;
+        resolve();
+      }, ms);
+    });
+  }
 }
 
 module.exports = ScreenController;
-
-function kaddy(phidza, zangalelo, marindi) {
-
-}
